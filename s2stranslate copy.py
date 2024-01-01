8d1b781f96d5324cf4c602dadd99e821c8544ee4
aranao00 <150121196+aranao00@users.noreply.github.com>
2024-01-01 15:41:14 +0900

    Add files via upload
    Feat: "Back translate 추가"

--- s2stranslate copy.py
+++ s2stranslate copy.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 import torchaudio
+
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.c0=nn.Conv2d(3, 25, kernel_size=3, stride=1, padding=1)
         self.c1=nn.Conv2d(25, 80, kernel_size=3, stride=1, padding=1)
         self.enc=torchaudio.models.Conformer()
-    def encode(self, frameseq):
+    def forward(self, frameseq):
         latent=self.c0(frameseq)
         latent=self.enc(latent)
         return latent
@@ -29,9 +30,10 @@ class Reconstruct(nn.Module):
         self.upconv1=nn.ConvTranspose2d()
 
 
-def train_Language_Embedding(textembedder, data):
+def train_Encoders(textembedder, data):
     enc=Encoder()
     enc_l=Decoder_L()
+
     textemb=textembedder
     embedding=enc(data)
     embedding=enc_l(embedding)
@@ -43,9 +45,7 @@ def train_Language_Embedding(textembedder, data):
     loss.backward()
     optim.step()
 
-
-
-def train_reconstruction():
+def train_Reconstruction():
     enc=Encoder()
     enc_a=Decoder_A()
     enc_l=Decoder_L()
@@ -62,4 +62,21 @@ def train_reconstruction():
     loss.backward()
     optim.step()
 
-def train_backtranslate():
+def train_Backtranslate(data):
+    enc_kr=Encoder()
+    enc_en=Encoder()
+    dec_kr=Reconstruct()
+    dec_en=Reconstruct()
+
+    data_pre=enc_kr(data)
+    seq=dec_en(data_pre)
+    data_post=enc_en(seq)
+    seq=dec_kr(data_post)
+
+    lossfn=nn.CrossEntropyLoss()
+    optim=torch.optim.Adam(dec_kr.parameters(), dec_en, enc_kr, enc_en, 0.0001)
+
+    loss=lossfn(data, data_post)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
